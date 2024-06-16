1b23189fd68b96b684a8e2df304c24b542c83f93
BrianFigueroa <bryan09figueroa@gmail.com>
2024-06-15 23:57:07 -0300

    edge add form

--- app/add-song/page.tsx
+++ app/add-song/page.tsx
@@ -1,12 +1,9 @@
 import AddSong from '@/components/create-form';
-import { Suspense } from 'react'
-import Loading from './loading';
-export const runtime = 'edge';
 
 export default function page() {
   return (
-    <Suspense fallback={<Loading />}>
+
       <AddSong />
-    </Suspense>
+
   )
 }
